9b7a5bb5c1b52446d0c587f9d74e8a0680ce46e6
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-12-23 16:14:21 -0800

    project structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title','Home')
+
+@section('sidebar')
+
+@endsection
+
+@section('content')
+<p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+     A minus magni aliquam, vitae perferendis deserunt.</p>
+@endsection
---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,5 @@
+@section('sidebar')
+<div class="sidebar">
+    <p class="lead">This is master side bar</p>
+    @show
+</div>
